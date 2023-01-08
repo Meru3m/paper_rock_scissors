ca4d1474ad101eb45bb36f32645e059bacf4c50e
Mattia Baldari <mattia.baldari@nttdata.com>
2023-01-08 16:12:50 +0100

    All tests passed

--- gamepage/tests.py
+++ gamepage/tests.py
@@ -1,3 +1,79 @@
+import json
 from django.test import TestCase
+from gamepage import views
 
-# Create your tests here.
+# Rock = 0
+# Paper = 1
+# Scissors = 2
+
+# Winner = 0 == no winner
+# Winner = 1 == User
+# Winner = 2 == Opponent
+
+class ResultViewTestCases(TestCase):
+    def setUp(self) -> None:
+        super().__init__()
+        self.url = "/result/"
+
+    def test_user_rock_opponent_rock(self):
+        body = json.dumps({'user_choice': 0, 'opponent_choice': 0})
+
+        result = self.client.post(self.url, body, content_type='application/json')
+        winner = json.loads(result.content).get("winner")
+        self.assertEqual(winner, 0)
+
+    def test_user_rock_opponent_paper(self):
+        body = json.dumps({'user_choice': 0, 'opponent_choice': 1})
+
+        result = self.client.post(self.url, body, content_type='application/json')
+        winner = json.loads(result.content).get("winner")
+        self.assertEqual(winner, 2)
+
+    def test_user_rock_opponent_scissors(self):
+        body = json.dumps({'user_choice': 0, 'opponent_choice': 2})
+
+        result = self.client.post(self.url, body, content_type='application/json')
+        winner = json.loads(result.content).get("winner")
+        self.assertEqual(winner, 1)
+
+    def test_user_paper_opponent_paper(self):
+        body = json.dumps({'user_choice': 1, 'opponent_choice': 1})
+
+        result = self.client.post(self.url, body, content_type='application/json')
+        winner = json.loads(result.content).get("winner")
+        self.assertEqual(winner, 0)
+
+    def test_user_paper_opponent_scissors(self):
+        body = json.dumps({'user_choice': 1, 'opponent_choice': 2})
+
+        result = self.client.post(self.url, body, content_type='application/json')
+        winner = json.loads(result.content).get("winner")
+        self.assertEqual(winner, 2)
+
+    def test_user_paper_opponent_rock(self):
+        body = json.dumps({'user_choice': 1, 'opponent_choice': 0})
+
+        result = self.client.post(self.url, body, content_type='application/json')
+        winner = json.loads(result.content).get("winner")
+        self.assertEqual(winner, 1)
+
+    def test_user_scissors_opponent_scissors(self):
+        body = json.dumps({'user_choice': 2, 'opponent_choice': 2})
+
+        result = self.client.post(self.url, body, content_type='application/json')
+        winner = json.loads(result.content).get("winner")
+        self.assertEqual(winner, 0)
+
+    def test_user_scissors_opponent_rock(self):
+        body = json.dumps({'user_choice': 2, 'opponent_choice': 0})
+
+        result = self.client.post(self.url, body, content_type='application/json')
+        winner = json.loads(result.content).get("winner")
+        self.assertEqual(winner, 2)
+
+    def test_user_scissors_opponent_paper(self):
+        body = json.dumps({'user_choice': 2, 'opponent_choice': 1})
+
+        result = self.client.post(self.url, body, content_type='application/json')
+        winner = json.loads(result.content).get("winner")
+        self.assertEqual(winner, 1)
--- gamepage/views.py
+++ gamepage/views.py
@@ -8,26 +8,23 @@ class ResultView(View):
         body_unicode = request.body.decode('utf-8')
         body = json.loads(body_unicode)
 
-        print(body)
-
         user_choise = body.get("user_choice")
         opponent_choise = body.get("opponent_choice")
         user_choise_mod = user_choise % 2
         opponent_choise_mod = opponent_choise % 2
 
-        winner = None
-
-        if user_choise_mod < opponent_choise_mod:
-            winner = 1
-        elif opponent_choise_mod < user_choise_mod:
-            winner = 0
-        elif opponent_choise_mod == user_choise_mod:
-            if user_choise < opponent_choise:
-                winner = 0
-            else:
+        winner = 0
+        if user_choise < opponent_choise:
+            winner = 2
+            if opponent_choise - user_choise == 2:
                 winner = 1
+        elif opponent_choise < user_choise:
+            winner = 1
+            if user_choise - opponent_choise == 2:
+                winner = 2
 
         response = JsonResponse({"winner": winner})
+
         return response
 
 
